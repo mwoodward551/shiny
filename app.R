db683aa1796dadaea978cb655e08fc4dfd4c3e4a
brentscott93 <brentscott93@gmail.com>
2019-10-28 09:53:22 -0400

    add mini ensemble. add detrend, dygraph add tabs for single molecule and reg. motility

--- app.R
+++ app.R
@@ -2,67 +2,114 @@ library(shiny)
 library(tidyverse)
 library(biophysr)
 library(shinythemes)
+library(gtools)
+library(gridExtra)
+library(dygraphs)
+library(shinycssloaders)
 
 
 # Define UI for app  ----
 ui <- fluidPage(theme = shinytheme("spacelab"),
 
-  navbarPage("Muscle Biophysics Lab"),
-
-  # App title ----
-  titlePanel("Automated Motility Analysis"),
-
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
-
-    # Sidebar panel for inputs ----
-    sidebarPanel(
-
-      # Input: File Input for uploading raw motility sheets ----
-      fileInput(inputId = "file",
+  navbarPage("Muscle Biophysics Lab",
+             
+    #Unregulated Motility Panel
+    tabPanel("Unregulated Motility",
+       sidebarLayout(
+        sidebarPanel(
+           fileInput(inputId = "file",
                   label = "Upload motility files (.xls)",
                   multiple = TRUE,
                   accept = ".xls",
                   buttonLabel = "Browse...",
                   placeholder = "No files selected"),
 
-      radioButtons(inputId = "myo_type",
-      label = "Myosin Type:",
-      choiceNames = list("Skeletal", "Cardiac"),
-      choiceValues = list("skeletal", "cardiac"),
-      inline = TRUE),
+           radioButtons(inputId = "myo_type",
+                   label = "Myosin Type:",
+                   choiceNames = list("Skeletal", "Cardiac"),
+                   choiceValues = list("skeletal", "cardiac"),
+                   inline = TRUE),
 
-      actionButton(inputId = "action_button",
+           actionButton(inputId = "action_button",
                    label = "Analyze",
                    width = 175),
 
-      downloadButton("download_by_video",
-                     "Download 'By Video'"),
+           downloadButton("download_by_video",
+                           "Download 'By Video'"),
 
-      downloadButton("download_summary",
-                     "Download Summary")
-    ),
+           downloadButton("download_summary",
+                         "Download Summary")
+                   ), #sidebar panel
 
 
-    # Main panel for displaying outputs ----
-    mainPanel(
+    
+        mainPanel(
 
-      tabsetPanel(type = "tabs",
+            tabsetPanel(type = "tabs",
                   tabPanel("By Video", tableOutput("video_table_output")),
                   tabPanel("Summary", tableOutput("summary_table_output"))
 
-      )
-
-
-
-    )
-  ))
+                 )
+             ) #main panel close
+         ) #side bar layout close
+ ), #motility tab Panel close
+ 
+ #Regulated Motility Tab
+  tabPanel("Regulated Motility",
+          print("This is for regulation")
+          ), #reguated tab close
+ 
+  #Mini_ensmble trap Tab
+  tabPanel("Mini-Ensemble Trap",
+          
+           sidebarLayout(
+             sidebarPanel(
+             fileInput(inputId = "mini_file",
+                     label = "Upload Mini-Ensemble Data (.txt)",
+                     multiple = FALSE,
+                     accept = ".txt",
+                     buttonLabel = "Browse...",
+                     placeholder = "No files selected"),
+             
+           #  actionButton(inputId = "view_mini_raw",
+            #              label = "View Raw Trace"),
+             
+             checkboxInput(inputId = "detrend_mini",
+                           label = "Detrend",
+                           value = FALSE),
+              actionButton(inputId = "mini_action_button",
+                       label = "Analyze")
+           
+          
+             ),
+                  
+             
+             mainPanel(
+               
+               tabsetPanel(type = "tabs",
+                           tabPanel("Raw Plot", dygraphOutput("explore_mini_raw") %>% withSpinner()),
+                           tabPanel("Measured Events", tableOutput("mini_table_output") %>% withSpinner()),
+                           tabPanel("Plot", plotOutput("mini_plot") %>% withSpinner())
+                           
+               )
+             ) #main panel close
+    
+             )
+  ), #mini-ensemble tab close
+  
+  tabPanel("Single-Molecule Trap",
+           print("This is for single molecule analysis")
+           ) #single molecule tab panel close
+        
+)#navbar page close
+)#fluid page close
 
 
 
 # Define server logic required to draw a histogram ----
 server <- function(input, output) {
 
+  options(shiny.maxRequestSize=30*1024^2)
 
 
   data_by_video <- eventReactive(input$action_button,{
@@ -169,7 +216,142 @@ server <- function(input, output) {
     content = function(file1) {
       write.csv(data_summary(), file1, row.names = FALSE)
     })
-}
+  
+  ###################### MINI ANALYSIS ######################
+  
+  
+ # mini_raw_data <- reactive({
+ ## mini <- scan(input$mini_file$datapath)
+  #})
+  
+  explore_mini <- reactive({ 
+    
+    mini <- scan(input$mini_file$datapath)
+    
+    if(input$detrend_mini == FALSE){
+    
+    raw_mini_trace <- data.frame(x = 1:length(mini),
+                                 y = mini)
+    
+
+    } else if(input$detrend_mini == TRUE){
+      
+      mini_detrend <- detrend_trap(mini, 25000)
+    
+      raw_mini_trace <- data.frame(x = 1:length(mini_detrend),
+                                 y = mini_detrend)
+      
+    }
+    
+    
+    mini_raw_plot <- dygraph(raw_mini_trace) %>% 
+      dyRangeSelector() %>% 
+      dyOptions(colors = "black") 
+    
+   return(mini_raw_plot)
+    
+    
+  })
+  
+
+
+  
+  
+  output$explore_mini_raw <- renderDygraph({
+    if(is.null(input$mini_file)){
+      
+      return(NULL)
+      
+    } else {
+      
+      return(explore_mini())
+    }
+  })
+  
+  mini_data <- eventReactive(input$mini_action_button,{
+  
+    
+   #mini <- scan(input$mini_file$datapath)
+    
+    mini <- scan(input$mini_file$datapath)
+    
+    if(input$detrend_mini == FALSE){
+      
+      analyzed_trap <- analyze_mini_ensemble(mini)
+      
+      
+    } else if(input$detrend_mini == TRUE){
+      
+      mini_detrend <- detrend_trap(mini, 25000)
+      
+      analyzed_trap <- analyze_mini_ensemble(mini_detrend)
+      
+    }
+
+    return(analyzed_trap)
+    
+    
+  })
+
+
+  plot_mini <- eventReactive(input$mini_action_button,{ 
+    
+    mini <- scan(input$mini_file$datapath)
+    
+    
+    if(input$detrend_mini == FALSE){
+      
+      plots <- plot_analyze_mini_ensemble(mini)
+      
+      
+      
+    } else if(input$detrend_mini == TRUE){
+      
+      mini_detrend <- detrend_trap(mini, 25000)
+      plots <- plot_analyze_mini_ensemble(mini_detrend)
+      
+      
+    }
+    
+
+    plot(plots)
+  
+ 
+        
+    
+    
+  })
+  
+
+
+  output$mini_table_output <- renderTable({
+    if(is.null(input$mini_file)){
+      
+    return (tibble("No data currently selected" =  "Please use 'Browse...' to upload"))
+      
+    } else {
+     return(mini_data())
+    }
+  })
+  
+ output$mini_plot <- renderPlot({
+    if(is.null(input$mini_file)){
+      
+     return(NULL)
+      
+    } else {
+      
+    return(plot_mini())
+    }
+ })
+
+  
+  
+  ###################### MINI END ##################
+  
+  
+  
+} # server close
 
 shinyApp(ui = ui, server = server)
 
